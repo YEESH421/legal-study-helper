fa4aae87c092284592c28d72a44537771988c937
tony zhou <tzhou49@gmail.com>
2023-06-02 23:15:31 -0400

    setup hello world flask server

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # legal-study-helper
 Helper tool that automates the process of extracting key features in peer-reviewed studies for legal experts to quickly sort/find evidence for cases.
+
+## Start backend (Python/flask)
+This assumes you already have a working installation of Python/pip. Guide comes from https://www.geeksforgeeks.org/flask-creating-first-simple-application/. First install virtualenv using pip ```pip install virtualenv```. Then, create a virtual Python virtual environment ```virtualenv venv```. Next, install Flask ```pip install Flask```, and run ```{your python alias} main.py```.
--- main.py
+++ main.py
@@ -1,28 +1,24 @@
-from PyPDF2 import PdfReader
-import enchant
-
-def visitor_body(text, cm, tm, font_dict, font_size):
-    y = tm[5]
-    if y > 10 and y < 780:
-        parts.append(text)
-
-def format(text):
-    return text
-
-
-dictionary = enchant.Dict("en_US")
-reader = PdfReader("paper2.pdf")
-number_of_pages = len(reader.pages)
-textList = [""] * number_of_pages
-for i in range(number_of_pages):
-    parts = []
-    reader.pages[i].extract_text(visitor_text=visitor_body)
-    text_body = "".join(parts)
-    text = format(text_body)
-    textList[i] = text_body
-f = open("test.txt", "w")
-f.write(textList[1])
-f.close()
-
-
-
+# from PyPDF2 import PdfReader
+# import openai
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from flask import Flask
+ 
+# Flask constructor takes the name of
+# current module (__name__) as argument.
+app = Flask(__name__)
+ 
+# The route() function of the Flask class is a decorator,
+# which tells the application which URL should call
+# the associated function.
+@app.route('/')
+# ‘/’ URL is bound with hello_world() function.
+def hello_world():
+    return 'Hello World'
+ 
+# main driver function
+if __name__ == '__main__':
+ 
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run()
